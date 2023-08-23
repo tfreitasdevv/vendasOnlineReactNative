73f1f04c95d7b3e12cfcbec9c09a96bf3a67789a
Thiago Freitas <tfreitasdevv@gmail.com>
2023-08-23 19:43:37 -0300

    Temporário. useEffects (hooks)
    Arquivo temporário para estudar useEffects

--- src/Example.tsx
+++ src/Example.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { Button, Text, View } from "react-native";
 
 interface ExampleProps {
@@ -10,6 +10,10 @@ const Example = ({text, children}: ExampleProps) => {
 
     // useState para gerenciar estado. Ao declarar a variável no array, toda vez que ela for alterada através do set..., o componente que a contém será renderizado.
     const [newText, setNewText] = useState();
+
+    useEffect(() => {
+        console.log('useEffect usado'); // Ação a ser executada na primeira renderização e toda vez que o estado dos itens do array forem alterados.
+    }, [newText]); // Se esse array for declarado vazio, a ação só executada na primeira vez.
 
     const handleOnPress = () => {
         setNewText('Novo Texto');
