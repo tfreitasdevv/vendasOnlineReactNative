9170ca0a2e7ec19ea746eacdc1798e3b99d6317b
Thiago Freitas <tfreitasdevv@gmail.com>
2023-08-23 15:49:40 -0300

    Styled-components

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,16 @@
 import React from "react";
 import { SafeAreaView, Text } from "react-native";
+import styled from "styled-components/native";
+
+const TextNew = styled.Text ` 
+    color: red;
+    font-size: 24px; 
+    `;
 
 const App = () => {
     return <SafeAreaView>
         <Text>Testando</Text>
+        <TextNew>NOVO TESTE</TextNew>
     </SafeAreaView>
 }
 
