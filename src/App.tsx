aa17dbf25da66b6f43dcde5fe0cd7f74f423e274
Thiago Freitas <tfreitasdevv@gmail.com>
2023-08-23 15:03:46 -0300

    Alterando o App.tsx

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { SafeAreaView, Text } from "react-native";
+
+const App = () => {
+    return <SafeAreaView>
+        <Text>Testando</Text>
+    </SafeAreaView>
+}
+
+export default App;
